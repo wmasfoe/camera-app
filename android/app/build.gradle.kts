178b382ffbb1ef31d343a445b853f65e1c820147
wmasfoe <lijq1103@gmail.com>
2026-09-18 19:59:53 +0000

    fix: 添加 material-icons-extended 依赖

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
